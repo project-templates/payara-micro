000ebd854282552811b9aa1a63732f249e716926
opengl8080 <tomcat.port.8080+github@gmail.com>
2016-01-12 23:43:53 +0900

    間違えて別のリポジトリにコミットしてしまった...
    This reverts commit 6894162152cdbb8481ed07c02b64662e0dd0e987.

--- src/main/java/sample/doma2/javaee/MyApplication.java
+++
@@ -1,9 +0,0 @@
-package sample.doma2.javaee;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("api")
-public class MyApplication extends Application {
-
-}
